4e5fd604b8690b11197d3f0fbcf607e03e722229
Jayaprakash-04 <Jayaprakash.NarayananS@cognizant.com>
2014-02-04 18:54:38 +0530

    .

--- src/main/java/org/hpccsystems/dashboard/entity/chart/utils/ChartRenderer.java
+++ src/main/java/org/hpccsystems/dashboard/entity/chart/utils/ChartRenderer.java
@@ -26,7 +26,9 @@ import org.xml.sax.SAXException;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
 import org.zkoss.zk.ui.util.Clients;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
 
 @VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
@@ -72,15 +74,20 @@ public class ChartRenderer {
 		final JsonArray array = new JsonArray();
 		
 		final JsonObject header = new JsonObject();
+		StringBuilder yName = new StringBuilder();
+		StringBuilder title = new StringBuilder();
 		if(chartData.getYColumnNames().size() > 0 && 
 				chartData.getXColumnNames().size() > 0) {
 			header.addProperty("xName", chartData.getXColumnNames().get(0));
-			header.addProperty("yName", chartData.getYColumnNames().get(0));
 			
-			if(chartData.getYColumnNames().size() > 1) {
-				header.addProperty("yName2", chartData.getYColumnNames().get(1));
+			for (String colName : chartData.getYColumnNames()) {
+				yName.append(colName);
+				yName.append(" & ");
 			}
+			header.addProperty("yName", yName.toString().substring(0, yName.toString().lastIndexOf('&') - 1));
 		}
+		title.append(chartData.getXColumnNames().get(0) + " BY " + yName.toString());
+		
 		if(isEditWindow) {
 			header.addProperty("portletId", "e_" + portlet.getId());
 		} else {
@@ -96,12 +103,19 @@ public class ChartRenderer {
 			header.addProperty("filterColumn", chartData.getFilter().getColumn());
 			header.addProperty("stringFilter", 
 					constructFilterTitle(chartData.getFilter().getValues()));
+			
+			title.append(" WHERE " + chartData.getFilter().getColumn());
+			title.append(constructFilterTitle(chartData.getFilter().getValues()));
 		
 		} else if (chartData.getIsFiltered() && 
 				Constants.NUMERIC_DATA.equals(chartData.getFilter().getType())) {
 			header.addProperty("filterColumn", chartData.getFilter().getColumn());
 			header.addProperty("from", chartData.getFilter().getStartValue());
 			header.addProperty("to", chartData.getFilter().getEndValue());
+			
+			title.append(" WHERE " + chartData.getFilter().getColumn());
+			title.append(" BETWEEN " + chartData.getFilter().getStartValue());
+			title.append(" AND " + chartData.getFilter().getEndValue());
 		}
 		
 				if(LOG.isDebugEnabled()){
@@ -123,14 +137,40 @@ public class ChartRenderer {
 			Integer xLength = 0;
 			String xVal=null;BigDecimal yVal=null;
 			JsonObject json = null;
+			
+			JsonArray xValues = new JsonArray();
+			
+			JsonArray rows = new JsonArray();
+			JsonArray row = new JsonArray();
+			
+			for (String colName : chartData.getYColumnNames()) {
+				row.add(new JsonPrimitive(colName));
+			}
+			rows.add(row);
+			
 			if(iterator != null){
 			while(iterator.hasNext()){
 				final XYModel bar = iterator.next();
+				row = new JsonArray();
+				for (Object object: bar.getyAxisValues()) {
+					row.add(new JsonPrimitive((BigDecimal)object));
+				}
+				
+				rows.add(row);
+				xValues.add(new JsonPrimitive(bar.getxAxisVal().toString()));
+				
 				json = new JsonObject();
 				json.addProperty("xData",(String) bar.getxAxisVal());
 				
 				//TODO - make this logic dynamic
 				if(Constants.BAR_CHART.equals(portlet.getChartType())){
+					JsonObject yNames = new JsonObject();
+					for (String colName : chartData.getYColumnNames()) {
+						yNames.addProperty(colName, "bar");
+					}
+					
+					header.add("yNames", yNames);
+					
 					json.addProperty(chartData.getYColumnNames().get(0), (BigDecimal)bar.getyAxisValues().get(0));
 					if(bar.getyAxisValues().size() > 1) {
 						json.addProperty(chartData.getYColumnNames().get(1), (BigDecimal)bar.getyAxisValues().get(1));
@@ -165,8 +205,11 @@ public class ChartRenderer {
 			//Adding a default pading of 5 and 10px per digit
 			header.addProperty("yWidth", (yLength<2)? yLength*10 + 30:(yLength<3)? yLength*10 + 10: yLength*10);
 			header.addProperty("xWidth", xLength*15 + 5);
+			header.addProperty("title", title.toString());
 			
 			header.add("chartData", array);
+			header.add("yValues", rows);
+			header.add("xValues", xValues);
 			
 			final String data = header.toString();
 			
@@ -210,7 +253,7 @@ public class ChartRenderer {
 	 */
 	private String constructFilterTitle(List<String> filteredValues)
 	{
-		StringBuffer stringFilterValue = new StringBuffer(" is ");
+		StringBuffer stringFilterValue = new StringBuffer(" IS ");
 		int index=0;
 		for(String filter :filteredValues)
 		{
