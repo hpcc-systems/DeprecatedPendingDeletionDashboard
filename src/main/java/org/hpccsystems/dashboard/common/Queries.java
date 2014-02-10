203291e56b8c53a85e12129632d66595578b6243
Jayaprakash-04 <Jayaprakash.NarayananS@cognizant.com>
2014-02-10 17:39:33 +0530

    .

--- src/main/java/org/hpccsystems/dashboard/common/Queries.java
+++ src/main/java/org/hpccsystems/dashboard/common/Queries.java
@@ -6,8 +6,7 @@ package org.hpccsystems.dashboard.common;
  */
 public class Queries {
 	public static final String GET_MAX_DASHBOARD_ID = "select max(dashboard_id) from dashboard_details where user_id=?";
-
-	public static final String INSERT_DASHBOARD = "INSERT INTO dashboard_details(dashboard_name,user_id,APPLICATION_ID,SOURCE_ID,updateddate,column_count) VALUES(?,?,?,?,?,?)";
+	public static final String INSERT_DASHBOARD = "INSERT INTO dashboard_details(dashboard_name,user_id,APPLICATION_ID,SOURCE_ID,updateddate,column_count,sequence) VALUES(?,?,?,?,?,?,?)";
 	public static final String UPDATE_DASHBOARD_SEQUENCE = "update dashboard_details set sequence=?, dashboard_name=? where dashboard_id=?";
 	public static final String DELETE_DASHBOARD_WIDGETS = "delete from widget_details where DASHBOARD_ID=?";
 	public static final String DELETE_WIDGETS = "delete from widget_details where WIDGET_ID=?";
@@ -15,6 +14,7 @@ public class Queries {
 	public static final String UPDATE_DASHBOARD_STATE = "update dashboard_details set dashboard_state=?,sequence=?,dashboard_name=?,updateddate=? where dashboard_id=?";
 	public static final String UPDATE_DASHBOARD_DETAILS = "update dashboard_details set sequence=?,dashboard_name=?,column_count=?,updateddate=? where dashboard_id=?";
 	public static final String UPDATE_WIDGET_DETAILS = "update widget_details set WIDGET_NAME=?,WIDGET_STATE=?,CHART_TYPE=?,COLUMN_IDENTIFIER=?,WIDGET_SEQUENCE=?,CHART_DATA=? where WIDGET_ID=? and DASHBOARD_ID=?";
+	public static final String UPDATE_WIDGET_SEQUENCE = "update widget_details set COLUMN_IDENTIFIER=?,WIDGET_SEQUENCE=? where WIDGET_ID=?";
 	public static final String INSERT_WIDGET_DETAILS = "insert into WIDGET_DETAILS(DASHBOARD_ID,WIDGET_NAME,WIDGET_STATE,CHART_TYPE,COLUMN_IDENTIFIER,WIDGET_SEQUENCE,CHART_DATA) values(?,?,?,?,?,?,?)";
 	public static final String GET_USER_DETAILs = "select user_id,password,active_flag,user_name from user_details where user_name=?"; 
 	public static final String GET_APPLICATIONS = "SELECT dash_app_id,dash_app_name FROM dash_application";
@@ -24,5 +24,6 @@ public class Queries {
 	public static final String GET_WIDGET = "select * from widget_details where DASHBOARD_ID=? order by WIDGET_SEQUENCE";
 	public static final String RETRIEVE_DASHBOARD_DETAILS = "select * from dashboard_details where APPLICATION_ID = ";
 	public static final String DASHBOARD_IN_CLAUSE = " and DASHBOARD_ID in ";
+	public static final String UPDATE_SIDEBAR_DETAILS = "update dashboard_details set sequence=? where dashboard_id=?";
 
 }
