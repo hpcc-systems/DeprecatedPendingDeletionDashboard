e9cad1517a62b5162b4b8d2e57da05b1788026a0
dhanasiddharth <dhanasiddharth@gmail.com>
2014-12-24 12:33:21 +0530

    Added dashboard class

--- src/main/java/org/hpccsystems/dashboard/entity/Dashboard.java
+++ src/main/java/org/hpccsystems/dashboard/entity/Dashboard.java
@@ -1,5 +1,35 @@
 package org.hpccsystems.dashboard.entity;
 
-public class Dashboard {
+import java.time.LocalDateTime;
 
+public class Dashboard {
+    private String id;
+    private String name;
+    private LocalDateTime lastUpDateTime;
+    private boolean visiblity;
+    
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public LocalDateTime getLastUpDateTime() {
+        return lastUpDateTime;
+    }
+    public void setLastUpDateTime(LocalDateTime lastUpDateTime) {
+        this.lastUpDateTime = lastUpDateTime;
+    }
+    public boolean isVisiblity() {
+        return visiblity;
+    }
+    public void setVisiblity(boolean visiblity) {
+        this.visiblity = visiblity;
+    }
 }
