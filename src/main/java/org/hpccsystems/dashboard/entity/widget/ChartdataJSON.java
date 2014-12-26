70bbf0ee12adbff1eca6804eeb40b493f06aa2db
senthilkumar2githup <SenthilKumar.Thangavel@cognizant.com>
2014-12-26 12:38:53 +0530

    Adding java doc for ChartDataJSON.

--- src/main/java/org/hpccsystems/dashboard/entity/widget/ChartdataJSON.java
+++ src/main/java/org/hpccsystems/dashboard/entity/widget/ChartdataJSON.java
@@ -6,6 +6,18 @@ public class ChartdataJSON {
     List<String> columns;
     List<List<Object>> data;
     
+    /* Sample chart JSON to be supplied to visualization api for rendering the chart.
+     * 
+     * columns : ["Subject", "Year 1", "Year 2"]
+     * 
+     * data : [
+            ["Geography", 75, 68],
+            ["English", 45, 55],
+            ["Math", 98, 92],
+            ["Science", 66, 60]
+        ]
+     */
+    
     public List<String> getColumns() {
         return columns;
     }
