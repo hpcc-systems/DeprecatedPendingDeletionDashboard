7e73278564620eb26e16e29b2dadb4f79cffb536
dhanasiddharth <dhanasiddharth@gmail.com>
2014-12-26 12:52:39 +0530

    Filter type changed to list

--- src/main/java/org/hpccsystems/dashboard/entity/widget/Widget.java
+++ src/main/java/org/hpccsystems/dashboard/entity/widget/Widget.java
@@ -4,8 +4,22 @@ import java.util.List;
 
 public abstract class Widget {
     private String name;
-    private Filter filter;
+    private List<Filter> filters;
     
     public abstract List<String> getColumns();
     public abstract String generateSQL();
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public List<Filter> getFilters() {
+        return filters;
+    }
+    public void setFilters(List<Filter> filters) {
+        this.filters = filters;
+    }    
+    
 }
