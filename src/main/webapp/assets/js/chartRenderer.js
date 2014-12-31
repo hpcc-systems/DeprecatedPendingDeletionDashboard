75ce0d9f1c043d6a471915a0e4775987a813b0d9
dhanasiddharth <dhanasiddharth@gmail.com>
2014-12-31 15:43:16 +0530

    Created pie chart

--- src/main/webapp/assets/js/chartRenderer.js
+++ src/main/webapp/assets/js/chartRenderer.js
@@ -8,23 +8,25 @@ function createPreview(target, chartType, data) {
 
 		var actualData = JSON.parse(data);
 		console.log(actualData);
-		actualData = [["Geography", 75, 68],["English", 45, 55],["Math", 98, 92],["Science", 66, 60]];
-		console.log(actualData);
+		//actualData = [["Geography", 75, 68],["English", 45, 55],["Math", 98, 92],["Science", 66, 60]];
 		
 		require([ "src/c3/Pie", "src/c3/Line", "src/c3/Column" ], function(
 				C3Pie, C3Line, C3Column) {
 
 			console.log(actualData);
 			if (chartType == "pie") {
-				new C3Pie().target(target).data(actualData).render();
+				new C3Pie()
+					.target(target)
+					.data(actualData.data)
+					.render();
 			}
 
 			if (chartType == "line") {
-				new C3Line().target(target).data(actualData).render();
+				new C3Line().target(target).data(actualData.data).render();
 			}
 
 			if (chartType == "column") {
-				new C3Column().target(target).data(actualData).render();
+				new C3Column().target(target).data(actualData.data).render();
 			}
 
 		});
